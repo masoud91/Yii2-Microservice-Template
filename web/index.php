f4f2a63e35c26c65f7f64d093bb0a2959d5a45b1
Masoud <masoudaghaei.m.a@gmail.com>
2019-09-19 16:01:31 +0430

    apply api coverage using PhpBrowser and C3

--- web/index.php
+++ web/index.php
@@ -7,6 +7,10 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
+define('C3_CODECOVERAGE_ERROR_LOG_FILE', __DIR__.'../tests/_output/c3_error.log'); //Optional (if not set the default c3 output dir will be used)
+require(__DIR__ . '/../web/c3.php');
+define('MY_APP_STARTED', true);
+
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../config/main.php'),
     require(__DIR__ . '/../config/main-local.php')
